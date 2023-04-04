5169b72e1ce47459c9d42b5bcea81f187f8f1480
Ada Phillips <ragwafire99@gmail.com>
2023-04-04 00:06:13 -0400

    Fix script and service issues

--- src/modules/user_rename/filesystem/root/rename_script.sh
+++ src/modules/user_rename/filesystem/root/rename_script.sh
@@ -5,16 +5,13 @@ arg=("$@")
 BASE_USER="$1"
 NEW_USER="$(grep "1000" /etc/passwd | awk -F ':' '{print $1}')"
 
-#echo "${arg}"
-echo "$0"
-echo "$@"
-echo "$#"
 echo "Renaming script and service references from user=$BASE_USER to user=$NEW_USER:"
 
 for ((i=1;i<=$#-1;i++)) ; do
-    echo "References in ${arg[i]}"
-    sudo sed -i "s/\/home\/${BASE_USER}/\/home\/${NEW_USER}/g" ${arg[i]}
-    sudo sed -i "s/${BASE_USER}\s*$/${NEW_USER}/g" ${arg[i]}
+    FILE=$(echo ${arg[i]} | sed "s/\/home\/pi/\/home\/${NEW_USER}/")
+    echo "References in ${FILE}"
+    sudo sed -i "s/\/home\/${BASE_USER}/\/home\/${NEW_USER}/g" ${FILE}
+    sudo sed -i "s/${BASE_USER}\s*$/${NEW_USER}/g" ${FILE}
 done
 
 systemctl disable user_rename.service
