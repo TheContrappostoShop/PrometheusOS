7cc190cd5412cc1e2eb29527c9a117fdaac88e28
Ada Phillips <ragwafire99@gmail.com>
2023-05-24 10:02:25 -0400

    Update to use Odyssey API

--- src/modules/odyssey/filesystem/home/pi/odyssey/sl1_watcher.sh
+++ src/modules/odyssey/filesystem/home/pi/odyssey/sl1_watcher.sh
@@ -4,6 +4,7 @@ inotifywait -m --format "%w,%e,%f" -e create -e moved_to ~/printer_data/config/ 
    while IFS=',' read file_path file_event file_name; do
         if [[ $file_name == *.sl1 ]] ; then
             echo ${file_path}${file_name} event: ${file_event}
-            echo "ODYSSEY_START FILE=\"${file_path}${file_name}\"" >> ~/printer_data/gcodes/"${file_name}".gcode
+            cp ${file_path}${file_name}  ~/printer_data/gcodes/${file_name}
+            echo "ODYSSEY_START FILE=\"~/printer_data/gcodes/${file_name}\"" >> ~/printer_data/gcodes/"${file_name}".gcode
         fi
    done
