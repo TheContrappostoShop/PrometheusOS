8d74d40970b2146f85974784caa44c0479862b73
Ada Phillips <ragwafire99@gmail.com>
2023-06-06 11:36:58 -0400

    Modify sl1_watcher to only touch the fake files

--- src/modules/odyssey/filesystem/home/pi/odyssey/sl1_watcher.sh
+++ src/modules/odyssey/filesystem/home/pi/odyssey/sl1_watcher.sh
@@ -4,7 +4,7 @@ inotifywait -m --format "%w,%e,%f" -e create -e moved_to ~/printer_data/config/ 
    while IFS=',' read file_path file_event file_name; do
         if [[ $file_name == *.sl1 ]] ; then
             echo ${file_path}${file_name} event: ${file_event}
-            cp ${file_path}${file_name}  ~/printer_data/gcodes/${file_name}
-            echo "ODYSSEY_START FILE=\"~/printer_data/gcodes/${file_name}\"" >> ~/printer_data/gcodes/"${file_name}".gcode
+            mv ${file_path}${file_name}  ~/printer_data/gcodes/${file_name}
+            touch ~/printer_data/gcodes/"${file_name}".gcode
         fi
    done
